6e02ec89c2893b63a344dd282e78349627eef1b3
KempachiDZ <alexmelnyk48370@gmail.com>
2024-04-22 13:19:36 +0300

    I add description good and toast to buy product

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
+// Define a route handler for the root route
+app.get('/', (req, res) => {
+  // Redirect to the '/index' route
+  res.redirect('/index');
+});
+
+
 
 // Define a route handler for the root route
 app.get('/index', (req, res) => {
@@ -18,6 +25,11 @@ app.get('/index', (req, res) => {
 app.get('/profile', (req, res) => {
   // Render the 'index.ejs' template
   res.render('profile');
+});
+
+app.get('/description', (req, res) => {
+  // Render the 'index.ejs' template
+  res.render('description');
 });
 
 // Start the server and listen on port 3000
