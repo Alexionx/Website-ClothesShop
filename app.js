a76984734cd03ea6d9b69caff48bfe0b4fb5eb17
KempachiDZ <alexmelnyk48370@gmail.com>
2024-10-04 00:50:17 +0300

    Change app.js

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 
+app.get('/', (req, res) => {
+  res.redirect('/index');
+});
+
 app.get('/index', (req, res) => {
   res.render('index');
 });
