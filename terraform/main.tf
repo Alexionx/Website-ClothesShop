cb77d5a8b55325f2e04756b3f8a639cfb293d351
KempachiDZ <alexmelnyk48370@gmail.com>
2024-10-15 20:03:09 +0300

    Change files

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,8 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-00f251754ac5da7f0"
   instance_type = "t2.micro"
-  key_name      = "devops-practice-test-key"  
+  key_name      = "devops-practice-test-key"
+  vpc_security_group_ids = [aws_security_group.app_sg.id] # Прив'язка Security Group  
 
   root_block_device {
     volume_size = 8
@@ -30,3 +31,45 @@ resource "aws_instance" "app_server" {
 output "ec2_public_ip" {
   value = aws_instance.app_server.public_ip
 }
+
+# налаштування security group
+resource "aws_security_group" "app_sg" {
+  name        = "app_security_group"
+  description = "Security group for EC2 app"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Дозволяє SSH доступ
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Дозволяє HTTP доступ
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Дозволяє HTTPS доступ
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Дозволяє доступ на порт 3000
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Дозволяє весь вихідний трафік
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
